e919984ca2addf7c308bb7022f2bf1c74a6a08ec
OnlyAbdullh <abdallaalkasm9@gmail.com>
2025-03-24 10:29:54 +0300

    using Header,Query and Redirect decorators and use Route wildcards.

--- src/Cats/cats.controller.ts
+++ src/Cats/cats.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  HttpCode,
+  Header,
+  Redirect,
+  Query,
+} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
+  @HttpCode(201)
+  @Header('Cache-Control', 'no-store')
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @HttpCode(200)
+  @Get('find/*') //any path after the find word is works.
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version: string) {
+    if (version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    } else if (version === '6') {
+      return { url: 'https://docs.nestjs.com/v6/' };
+    }
   }
 }
